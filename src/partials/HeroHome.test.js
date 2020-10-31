e2a5c42a3292396f429907e71bd306cebb882cb8
usraptor2016 <wen.347@osu.edu>
2020-10-30 23:44:11 -0500

    uopasdfsfsd

--- src/partials/HeroHome.test.js
+++ src/partials/HeroHome.test.js
@@ -14,6 +14,7 @@ describe("School Camp Test Case", () => {
   
       expect(screen.findByText("Arrowed")).not.toBeNull();
       expect(screen.findByText("Camp")).not.toBeNull();
+      
       expect(
         screen.findAllByText("相信营地体验能给每一位孩子带来影响深远的宝贵经验，他们的目标是为每一位露营者提供尽可能最好的机会来促进他们在一个欢快的环境中学习和成长")
       ).not.toBeNull();
